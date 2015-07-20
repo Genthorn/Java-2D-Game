c72e382f16b7587b8584de5037c0ed38a8ad5ffe
Zormion <Zormion@gmail.com>
2015-07-20 18:04:44 -0400

    Started inventory

---
+++ src/com/zormion/game/huds/Inventory.java
@@ -0,0 +1,17 @@
+package com.zormion.game.huds;
+
+import com.zormion.game.gfx.Screen;
+
+public class Inventory {
+	public Inventory() {
+		
+	}
+	
+	public void update() {
+		
+	}
+	
+	public void render(Screen screen) {
+		//screen.render(xPos, yPos, tile, mirrorDir, scale);
+	}
+}
